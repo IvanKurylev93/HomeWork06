bebf8dfc54d52971e06eb92f19fcbf1fc9529731
IvanKurylev93 <kuiv10@mail.ru>
2022-02-19 12:38:10 +0300

    Задача 42

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 42: Определить, сколько чисел больше 0 введено с клавиатуры.
+int x=0;
+Console.WriteLine("Введите колличество цифр: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 1: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+x = Convert.ToInt32(Console.ReadLine());
+{
+    if (x >= 0)
+        Console.WriteLine("Положительных чисел: " + x);
+        Console.ReadLine();
+    }
+
+
+
